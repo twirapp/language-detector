1dd9e19b70ecb804bb399c9d4b9809cddd598874
Satont <satontworldwide@gmail.com>
2023-11-27 19:05:40 +0300

    Add available languages endpoint

--- internal/http/http.go
+++ internal/http/http.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	l "github.com/pemistahl/lingua-go"
 	"github.com/twirapp/language-detector/internal/lingua"
 )
 
@@ -40,11 +41,17 @@ func New(ctx context.Context, port string, lingua *lingua.Lingua) {
 }
 
 type responseLang struct {
-	Code     int `json:"code"`
-	Iso639_3 int `json:"iso_693_3"`
+	Code     int    `json:"code"`
+	Iso639_3 int    `json:"iso_693_3"`
+	Name     string `json:"name"`
 }
 
 func (c *myHttp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/languages" {
+		c.allLanguagesController(w, r)
+		return
+	}
+
 	text := r.URL.Query().Get("text")
 	if text == "" {
 		http.Error(w, "no text provded", http.StatusBadRequest)
@@ -59,6 +66,23 @@ func (c *myHttp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		resp[i] = responseLang{
 			Code:     int(lang.Language()),
 			Iso639_3: int(lang.Language().IsoCode639_3()),
+			Name:     lang.Language().String(),
+		}
+	}
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+	}
+}
+
+func (c *myHttp) allLanguagesController(w http.ResponseWriter, r *http.Request) {
+	resp := make([]responseLang, len(l.AllLanguages()))
+
+	for i, lang := range l.AllLanguages() {
+		resp[i] = responseLang{
+			Code:     int(lang),
+			Iso639_3: int(lang.IsoCode639_3()),
+			Name:     lang.String(),
 		}
 	}
 
